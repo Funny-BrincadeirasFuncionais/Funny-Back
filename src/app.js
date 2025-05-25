ab6e4defd37ab0d03a3fc342398a39e6d2a7e32d
FilipeGomesFreire <162646231+FilipeGomesFreire@users.noreply.github.com>
2025-05-25 20:06:15 +0000

    feat: finaliza todas as entidades e rotas base da API

--- src/app.js
+++ src/app.js
@@ -4,9 +4,25 @@ const sequelize = require('./database');
 
 app.use(express.json());
 
+//VAI COLAR AS CONSTANTES AQUI HO, DAQUI
+
 const Diagnostico = require('./models/Diagnostico'); // garante que o modelo seja carregado
 app.use('/diagnosticos', require('./routes/diagnosticos'));
 
+const Responsavel = require('./models/Responsavel');
+app.use('/responsaveis', require('./routes/responsaveis'));
+
+const Atividade = require('./models/Atividade');
+app.use('/atividades', require('./routes/atividades'));
+
+const Crianca = require('./models/Crianca');
+app.use('/criancas', require('./routes/criancas'));
+
+const Progresso = require('./models/Progresso');
+app.use('/progresso', require('./routes/progresso'));
+
+
+//ATÉ AQUI PELO AMOR DE DEUS NÃO COLOCA NADA FORA
 
 // Teste de rota
 app.get('/', (req, res) => {
